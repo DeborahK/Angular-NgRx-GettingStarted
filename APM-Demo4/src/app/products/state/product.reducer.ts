aaba97c6eb9967b7ede4e29fb2aeac3c10572174
Deborah Kurata <deborahk@insteptech.com>
2020-04-30 10:54:04 -0700

    Strongly type the on callback functions.

--- APM-Demo4/src/app/products/state/product.reducer.ts
+++ APM-Demo4/src/app/products/state/product.reducer.ts
@@ -82,33 +82,33 @@ const productReducer = createReducer<ProductState>(
       currentProductId: action.product.id
     };
   }),
-  on(ProductActions.clearCurrentProduct, state => {
+  on(ProductActions.clearCurrentProduct, (state): ProductState => {
     return {
       ...state,
       currentProductId: null
     };
   }),
-  on(ProductActions.initializeCurrentProduct, state => {
+  on(ProductActions.initializeCurrentProduct, (state): ProductState => {
     return {
       ...state,
       currentProductId: 0
     };
   }),
-  on(ProductActions.loadProductsSuccess, (state, action) => {
+  on(ProductActions.loadProductsSuccess, (state, action): ProductState => {
     return {
       ...state,
       products: action.products,
       error: ''
     };
   }),
-  on(ProductActions.loadProductsFailure, (state, action) => {
+  on(ProductActions.loadProductsFailure, (state, action): ProductState => {
     return {
       ...state,
       products: [],
       error: action.error
     };
   }),
-  on(ProductActions.updateProductSuccess, (state, action) => {
+  on(ProductActions.updateProductSuccess, (state, action): ProductState => {
     return {
       ...state,
       products: state.products.map(
@@ -117,14 +117,14 @@ const productReducer = createReducer<ProductState>(
       error: ''
     };
   }),
-  on(ProductActions.updateProductFailure, (state, action) => {
+  on(ProductActions.updateProductFailure, (state, action): ProductState => {
     return {
       ...state,
       error: action.error
     };
   }),
   // After a create, the currentProduct is the new product.
-  on(ProductActions.createProductSuccess, (state, action) => {
+  on(ProductActions.createProductSuccess, (state, action): ProductState => {
     return {
       ...state,
       products: [...state.products, action.product],
@@ -132,14 +132,14 @@ const productReducer = createReducer<ProductState>(
       error: ''
     };
   }),
-  on(ProductActions.createProductFailure, (state, action) => {
+  on(ProductActions.createProductFailure, (state, action): ProductState => {
     return {
       ...state,
       error: action.error
     };
   }),
   // After a delete, the currentProduct is null.
-  on(ProductActions.deleteProductSuccess, (state, action) => {
+  on(ProductActions.deleteProductSuccess, (state, action): ProductState => {
     return {
       ...state,
       products: state.products.filter(product => product.id !== action.productId),
@@ -147,7 +147,7 @@ const productReducer = createReducer<ProductState>(
       error: ''
     };
   }),
-  on(ProductActions.deleteProductFailure, (state, action) => {
+  on(ProductActions.deleteProductFailure, (state, action): ProductState => {
     return {
       ...state,
       error: action.error
@@ -155,6 +155,6 @@ const productReducer = createReducer<ProductState>(
   })
 );
 
-export function reducer(state: ProductState, action: Action):  ProductState {
+export function reducer(state: ProductState, action: Action) {
   return productReducer(state, action);
 }
