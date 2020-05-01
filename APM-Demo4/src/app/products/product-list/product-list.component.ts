7100ebf98659cbb423f31b4040678d22ba848c8c
Deborah Kurata <deborahk@insteptech.com>
2020-05-01 16:00:35 -0700

    Demo 0 changes

--- APM-Demo4/src/app/products/product-list/product-list.component.ts
+++ APM-Demo4/src/app/products/product-list/product-list.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Product } from '../product';
-
 import { Observable } from 'rxjs';
+
+import { Product } from '../product';
 
 /* NgRx */
 import { Store, select } from '@ngrx/store';
