e73e3031210e8c5d83903035546adf65a1324796
Florian Spier <Florian.Spier@portofantwerp.com>
2022-01-07 15:23:58 +0100

    WIP product state facade

--- src/app/products/product-shell/product-shell.component.ts
+++ src/app/products/product-shell/product-shell.component.ts
@@ -1,68 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-
 import { Product } from '../product';
-
-/* NgRx */
-import { Store } from '@ngrx/store';
-import { State, getShowProductCode, getCurrentProduct, getProducts, getError } from '../state';
-
-import { ProductPageActions } from '../state/actions';
+import { ProductStateFacadeService } from '../state/product-state-facade.service';
 
 @Component({
     templateUrl: './product-shell.component.html',
 })
 export class ProductShellComponent implements OnInit {
-    displayCode$: Observable<boolean>;
-    selectedProduct$: Observable<Product | undefined | null>;
-    products$: Observable<Product[]>;
-    errorMessage$: Observable<string>;
-
-    constructor(private store: Store<State>) {}
+   constructor(public productState: ProductStateFacadeService) {}
 
     ngOnInit(): void {
-        // Do NOT subscribe here because it uses an async pipe
-        // This gets the initial values until the load is complete.
-        this.products$ = this.store.select(getProducts);
-
-        // Do NOT subscribe here because it uses an async pipe
-        this.errorMessage$ = this.store.select(getError);
-
-        this.store.dispatch(ProductPageActions.loadProducts());
-
-        // Do NOT subscribe here because it uses an async pipe
-        this.selectedProduct$ = this.store.select(getCurrentProduct);
-
-        // Do NOT subscribe here because it uses an async pipe
-        this.displayCode$ = this.store.select(getShowProductCode);
+        this.productState.loadProducts();
     }
 
     checkChanged(): void {
-        this.store.dispatch(ProductPageActions.toggleProductCode());
+      this.productState.toggleProductCode();
     }
 
     newProduct(): void {
-        this.store.dispatch(ProductPageActions.initializeCurrentProduct());
+       this.productState.initializeCurrentProduct();
     }
 
     productSelected(product: Product): void {
-        this.store.dispatch(
-            ProductPageActions.setCurrentProduct({ currentProductId: product.id! })
-        );
+        this.productState.setCurrentProduct(product);
     }
 
     deleteProduct(product: Product): void {
-        this.store.dispatch(ProductPageActions.deleteProduct({ productId: product.id! }));
+      this.deleteProduct(product);
     }
 
     clearProduct(): void {
-        this.store.dispatch(ProductPageActions.clearCurrentProduct());
+      this.productState.clearCurrentProduct();
     }
+
     saveProduct(product: Product): void {
-        this.store.dispatch(ProductPageActions.createProduct({ product }));
+this.productState.createProduct(product);
     }
 
     updateProduct(product: Product): void {
-        this.store.dispatch(ProductPageActions.updateProduct({ product }));
+this.productState.updateProduct(product)
     }
 }
---
+++ src/app/products/state/product-state-facade.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Product } from '../product';
+import { getCurrentProduct, getError, getProducts, getShowProductCode } from './index';
+import { ProductPageActions } from './actions';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductStateFacadeService {
+
+  displayCode$: Observable<boolean> = this.store.select(getShowProductCode);
+  selectedProduct$: Observable<Product | undefined | null> = this.store.select(getCurrentProduct);
+  products$: Observable<Product[]> = this.store.select(getProducts);
+  errorMessage$: Observable<string> = this.store.select(getError);
+
+  constructor(
+    private store: Store
+  ) {
+  }
+
+  loadProducts(): void {
+    this.store.dispatch(ProductPageActions.loadProducts());
+  }
+
+  toggleProductCode(): void {
+    this.store.dispatch(ProductPageActions.toggleProductCode());
+  }
+
+  initializeCurrentProduct(): void {
+    this.store.dispatch(ProductPageActions.initializeCurrentProduct());
+  }
+
+  setCurrentProduct(product: Product): void {
+    this.store.dispatch(
+      ProductPageActions.setCurrentProduct({ currentProductId: product.id! })
+    );
+  }
+
+  deleteProduct(product: Product): void {
+    this.store.dispatch(ProductPageActions.deleteProduct({ productId: product.id! }));
+  }
+
+  clearCurrentProduct(): void {
+    this.store.dispatch(ProductPageActions.clearCurrentProduct());
+  }
+
+  createProduct(product: Product): void {
+    this.store.dispatch(ProductPageActions.createProduct({ product }));
+  }
+
+  updateProduct(product: Product): void {
+    this.store.dispatch(ProductPageActions.updateProduct({ product }));
+  }
+}
