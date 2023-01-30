7a5f4b7b0d53233f27f405b9f06bc76bca5d15a2
Vijay Kumar <vijay1021.gummadi@gmail.com>
2023-01-30 05:52:36 -0500

    NgrX added

--- APM-Demo0/src/app/app.module.ts
+++ APM-Demo0/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { PageNotFoundComponent } from './home/page-not-found.component';
 
 /* Feature Modules */
 import { UserModule } from './user/user.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   imports: [
@@ -23,7 +24,8 @@ import { UserModule } from './user/user.module';
     HttpClientModule,
     HttpClientInMemoryWebApiModule.forRoot(ProductData),
     UserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {})
   ],
   declarations: [
     AppComponent,
